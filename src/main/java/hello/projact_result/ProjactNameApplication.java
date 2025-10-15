252c93fc92449c50f923cfab3f78dfd45206f861
bangsinchur <sinchurbang123@gmail.com>
2025-10-15 16:55:19 +0900

    spring 기초

---
+++ src/main/java/hello/projact_result/ProjactNameApplication.java
@@ -0,0 +1,13 @@
+package hello.projact_result;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjactNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjactNameApplication.class, args);
+	}
+
+}
